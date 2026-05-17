12da4e993f7c6d4ab2c58577d8d829489255c52b
shauncode24 <shaunak.karve@somaiya.edu>
2026-05-17 18:19:33 +0530

    Frontend directory setup

---
+++ frontend/src/pages/LandingPage.jsx
@@ -0,0 +1,11 @@
+import './LandingPage.css';
+
+function LandingPage() {
+  return (
+    <div className="landing-page">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default LandingPage;
